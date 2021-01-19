cc0993dd1de0fa894da937dfd606cd71a62ae457
Cronut Assunta <cassunta@oa-j3.msk.21-school.ru>
2021-01-19 21:15:17 +0300

    st

--- get_cmd.c
+++ get_cmd.c
@@ -1,41 +1,5 @@
 #include "sh.h"
 
-/*int				is_redirect_here(char *s)
-{
-	int			i;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '<' || s[i] == '>')
-		{
-			if (s[i - 1] && s[i - 1] != 92)
-				return (1);
-		}
-		i++;
-	}
-}
-
-t_cmd			*data_for_exp(t_token *t, t_cmd *cmd)
-{
-	if (!is_redirect_here(t->data))
-		cmd->arr = ft_strsplit(t->data);
-	//else
-		//cmd = get_data_with_redirect(t->data, cmd);
-	return (cmd);
-}
-
-t_cmd			*get_data_cmd(t_token *t, t_cmd *cmd)
-{
-	if (t->type == 2)
-		cmd = data_for_exp(t, cmd);
-	if (t->type == 1)
-	{
-		if (cmd->prev)
-	}
-}*/
-
-
 t_cmd			*get_data_cmd(t_token *t, t_cmd *d)
 {
 	d->arr = ft_strsplit(t->data, ' ');
